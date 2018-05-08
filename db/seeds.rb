6bf164cb0d04c0dc5ab62b01c4ff39e7cf88ef11
Oscar <oscarwasil@gmail.com>
2018-05-08 18:44:29 -0500

    Newest products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
 product.save
 
-product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
+product = Products.new(name: "Ultralight chalk bag", price: 24, img_url: "http://www.blackdiamondequipment.com/on/demandware.static/-/Sites-bdel/default/dwcb526bda/products/chalk_chalkbags/630136_Mojo_Zip_RUST_L_web.jpg", description: "Inspired by the ultralight design of our backpacks, the Ultralight Chalk Bag maximizes weight savings and minimizes bulk, designed with Dynex fabric" )
 product.save
 
 product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
 product.save
 
-product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
+product = Products.new(name: "60m climbing rope", price: 120, img_url: "https://www.rei.com/media/product/8983540002", description: "Built to withstand heavy-use, the Black Diamond 9.9 is a burly cord for year-round rock climbing. Whether you’re roping up in the desert or tying in at your favorite summer sport crag, the 9.9 can handle it. " )
 product.save
 
-product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
+product = Products.new(name: "Impact crash pad", price: 199, img_url: "https://images-na.ssl-images-amazon.com/images/I/71fn8tcMRTL._AC_SR201,266_.jpg", description: "A simple, all-around pad for day-to-day or entry-level use, the Impact features a clean, hinge-fold design and a premium foam layup" )
 product.save
 
-product = Products.new(name: "Zone harness", price: 79, img_url: "https://i.stpost.com/black-diamond-equipment-aspect-climbing-harness-for-men-in-deep-blue~p~5206v_02~440~40.2.jpg", description: "The lightest Black Diamond harness built for routes that require maximum focus and minimum weight, the Zone is the ultimate sport climbing workhorse" )
+product = Products.new(name: "Vapor helmet", price: 100, img_url: "https://content.backcountry.com/images/items/900/BLD/BLD1346/LIM.jpg", description: "The lightest, most breathable and most comfortable climbing helmet we’ve ever made, the Vapor provides reliable, ultralight protection." )
 product.save
 
